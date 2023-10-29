e835d8508b0090d61376f566fd83f48cc21df238
ssghost <ssprof0@gmail.com>
2023-10-29 17:04:51 +0800

    finish 9

--- filters.py
+++ filters.py
@@ -1,8 +1,10 @@
 import pandas as pd
-import numpy as np  
+import numpy as np
+import pywt as wt  
 from pykalman import KalmanFilter
-from scipy.signal import butter, sosfilt
+from scipy.signal import butter, sosfilt, medfilt, savgol_filter
 from scipy.ndimage import gaussian_filter
+from scipy.fft import fft
 
 class Filter:
     def __init__(self) -> None:
@@ -14,12 +16,12 @@ class Filter:
     def expo_smooth(self) -> pd.DataFrame:
         return self.data.ewm(com=0.5).mean()
     
-    def kalman(self) -> (pd.DataFrame, pd.DataFrame):
+    def kalman(self) -> pd.DataFrame:
         measurements = np.asarray(enumerate(self.data))
         kf = KalmanFilter(transition_matrices = [[1, 1], [0, 1]], observation_matrices = [[0.1, 0.5], [-0.3, 0.0]])
         kf = kf.em(measurements, n_iter=5)
         (filtered_state_means, filtered_state_covariances) = kf.filter(measurements)
-        return pd.DataFrame(filtered_state_means), pd.DataFrame(filtered_state_covariances)
+        return pd.DataFrame({'fsm':filtered_state_means, 'fsc':filtered_state_covariances})
     
     def butterworth(self) -> pd.DataFrame:
         sig = np.asarray(self.data)
@@ -30,4 +32,15 @@ class Filter:
         sig = np.asarray(self.data).reshape(5,5)
         return pd.DataFrame(gaussian_filter(sig, sigma=1))
     
+    def median(self) -> pd.DataFrame:
+        return pd.DataFrame(medfilt(np.asarray(self.data), 5))
     
+    def wavelet(self) -> pd.DataFrame:
+        (ca, cd) = wt.dwt(np.asarray(self.data), 'db1') 
+        return pd.DataFrame({'ca':ca, 'cd':cd})
+    
+    def sav_gol(self) -> pd.DataFrame:
+        return pd.DataFrame(savgol_filter(np.asarray(self.data), 5, 2))
+    
+    def fourier(self) -> pd.DataFrame:
+        return pd.DataFrame(fft(np.asarray(self.data))) 

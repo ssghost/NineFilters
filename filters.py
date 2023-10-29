9be996e2deb3d8bf837365204c88fcfb8e62a7f5
ssghost <ssprof0@gmail.com>
2023-10-29 13:54:58 +0800

    add kalmann

--- filters.py
+++ filters.py
@@ -1,3 +1,13 @@
 import pandas as pd 
-import numpy as np 
 
+class Filter:
+    def __init__(self) -> None:
+        self.data = pd.DataFrame(pd.read_csv("sample.csv"))
+
+    def moving_average(self) -> pd.DataFrame:
+        return self.data.rolling(20).mean()
+    
+    def expo_smooth(self) -> pd.DataFrame:
+        return self.data.ewm(com=0.5).mean()
+    
+    def 
